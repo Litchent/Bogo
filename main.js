e34938caa4296a07c6f61ec399be67ab0a9e52b6
cristhian <cristhianmiguelr2@gmail.com>
2024-11-30 16:04:17 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// alert("hola");
+
+let message = "hola mundo 2";
+let message2 = "hola mundo 4";
+
+// alert(message);
+// alert(message2);
+
+let number1 = 12;
+let number2 = 21;
+
+// let (number1 + number2);
+
+let year = prompt("ingresa el ano")
---
+++ main2.js
@@ -0,0 +1,63 @@
+// // // Variables
+
+// // let MyName;
+// // MyName = "josefino";
+
+// // let MyAge = 15;
+// let MyName = "josefino2",
+//     MyAge = 15;
+//     MyMesssage = "hello :)";
+
+// // alert(MyName);
+// // alert(MyAge);
+// // alert(MyMessage);
+
+// // constantes
+// const MyBirthDay = "Apr-21-2014";
+
+// // alert(MyBirthDay);
+
+// // tipos de datos
+// let MyAge2 = 33;
+// let IsActive = true;
+// let IsBlue = false;
+
+// let MyHeight = null;
+// let MyHeight2 = undefined;
+
+// // ingreso de argumentos
+// let year = prompt('año actual:');
+// // alert(year);
+
+// // confirm
+// let isBoss = confirm("Eres el Jefe?");
+// // alert(isBoss);
+
+// // operadores matematicos
+// let Num1 = 10;
+// let Num2 = 20;
+
+// // // suma
+// // alert( Num1 + Num2);
+
+// // // resta 
+// // alert(Num1 - Num2);
+
+// // // multiplicacion
+// // alert(Num1 * Num2);
+
+// // // division
+// // alert(Num1 / Num2);
+
+// // incremento
+// let Num3 = 2;
+// Num3++;
+// alert(Num3);
+
+//  operadores de comparacion
+// alert( 2 > 1 );
+// alert( 2 < 1 );
+// alert( 2 == 1 );
+// alert( 2 != 1);
+
+// condicionales
